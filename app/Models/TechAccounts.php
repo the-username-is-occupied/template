f2b0a993c9b4673a8f38b058bf026cb7b4c30da0
Andrew <dolzhenko.andrew@gmail.com>
2026-06-09 09:47:16 +0300

    small

--- app/Models/TechAccounts.php
+++ app/Models/TechAccounts.php
@@ -61,11 +61,15 @@ class TechAccounts extends Model
     public function test(string $q = 'Кто такая Сильвана? Ответь коротко')
     {
         // sylvanas 664040dd-5608-490a-b35f-92b06979f768
-        //  return (new NotebookLMService)->listNotebooks($this->id);
+        return (new NotebookLMService)->listNotebooks($this->id);
         // return (new NotebookLMService)->getNotebook($this->id, "80be8d97-a00f-4ce1-a560-a424e65ee56d");
         return (new NotebookLMService)->setPublic($this->id, 'f05b4c43-97b7-4097-91b6-94c82226552b');
 
-        return (new NotebookLMService)->askQuestion($this->id, '80be8d97-a00f-4ce1-a560-a424e65ee56d', $q);
+    }
+
+    public function ask(string $q = 'Notebook')
+    {
+        return (new NotebookLMService)->askQuestion($this->id, '664040dd-5608-490a-b35f-92b06979f768', $q);
     }
 
     public function createnb(string $q = 'Notebook')
--- app/Utils/helpers.php
+++ app/Utils/helpers.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Models\TechAccounts;
+
 if (! function_exists('format_duration')) {
     function format_duration(float $duration): string
     {
         return $duration < 0.1 ? round($duration * 1000, 1).'ms' : round($duration, 2).'s';
+    }
+}
+
+if (! function_exists('tech')) {
+    function tech() 
+    {
+        return TechAccounts::query();
     }
 }
